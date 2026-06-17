4b7d28fa8ac62a974d5b20222ee6ff93dc17a49e
hector <hectorptvc@gmail.com>
2026-06-17 10:54:48 -0400

    feat: replace emoji icons with SVGs + hover animations in producciones-cooperadas

--- PaginaWeb/public/producciones-cooperadas.php
+++ PaginaWeb/public/producciones-cooperadas.php
@@ -27,22 +27,48 @@
                 </div>
                 <div class="grid">
                     <div class="card animate-on-scroll">
-                        <div class="card-icon">&#128230;</div>
+                        <div class="card-icon">
+                            <svg class="icon-recursos" width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <rect x="4" y="8" width="32" height="24" rx="2" stroke="white" stroke-width="2" fill="none"/>
+                                <path d="M4 14H36" stroke="white" stroke-width="2"/>
+                                <path d="M16 14V32" stroke="white" stroke-width="2"/>
+                                <path d="M24 14V32" stroke="white" stroke-width="2"/>
+                            </svg>
+                        </div>
                         <h4>Compartir Recursos</h4>
                         <p>Las empresas pueden compartir instalaciones, equipos y materias primas, lo que reduce costos y optimiza el uso de recursos.</p>
                     </div>
                     <div class="card animate-on-scroll delay-1">
-                        <div class="card-icon">&#127758;</div>
+                        <div class="card-icon">
+                            <svg class="icon-mercados" width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <circle cx="20" cy="20" r="14" stroke="white" stroke-width="2" fill="none"/>
+                                <ellipse cx="20" cy="20" rx="7" ry="14" stroke="white" stroke-width="2" fill="none"/>
+                                <path d="M6 14H34" stroke="white" stroke-width="1.5"/>
+                                <path d="M6 26H34" stroke="white" stroke-width="1.5"/>
+                            </svg>
+                        </div>
                         <h4>Acceso a Nuevos Mercados</h4>
                         <p>Al cooperar, las empresas pueden acceder a mercados que de otra manera ser&iacute;an dif&iacute;ciles de alcanzar, aumentando su competitividad.</p>
                     </div>
                     <div class="card animate-on-scroll delay-2">
-                        <div class="card-icon">&#128161;</div>
+                        <div class="card-icon">
+                            <svg class="icon-innovacion" width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M13 24C13 24 10 20 10 15C10 10 14 6 20 6C26 6 30 10 30 15C30 20 27 24 27 24H13Z" stroke="white" stroke-width="2" stroke-linejoin="round" fill="none"/>
+                                <line x1="15" y1="28" x2="25" y2="28" stroke="white" stroke-width="2" stroke-linecap="round"/>
+                                <line x1="17" y1="31" x2="23" y2="31" stroke="white" stroke-width="2" stroke-linecap="round"/>
+                                <line x1="20" y1="24" x2="20" y2="28" stroke="white" stroke-width="2"/>
+                            </svg>
+                        </div>
                         <h4>Innovaci&oacute;n y Desarrollo</h4>
                         <p>Permite el intercambio de conocimientos y tecnolog&iacute;as, fomentando la innovaci&oacute;n y el desarrollo de nuevos productos o servicios.</p>
                     </div>
                     <div class="card animate-on-scroll delay-3">
-                        <div class="card-icon">&#128737;</div>
+                        <div class="card-icon">
+                            <svg class="icon-riesgos" width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M20 4L6 12V22C6 30 12 36 20 38C28 36 34 30 34 22V12L20 4Z" stroke="white" stroke-width="2" stroke-linejoin="round" fill="none"/>
+                                <path d="M14 22L18 26L26 18" stroke="white" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </div>
                         <h4>Reducci&oacute;n de Riesgos</h4>
                         <p>Compartir los riesgos asociados con nuevos proyectos o mercados puede hacer que las inversiones sean m&aacute;s seguras y manejables.</p>
                     </div>
