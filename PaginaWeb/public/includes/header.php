229a74fec045e7180f6703722a28be2a126511df
epmjob00 <epmjob00@gmail.com>
2026-06-24 10:25:38 -0400

    perf: defer main.js, preload CSS and add basic caching/compression rules

--- PaginaWeb/public/includes/header.php
+++ PaginaWeb/public/includes/header.php
@@ -74,6 +74,7 @@ header("Permissions-Policy: geolocation=(), camera=(), microphone=(), midi=(), s
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
+    <link rel="preload" href="css/style.css" as="style">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
